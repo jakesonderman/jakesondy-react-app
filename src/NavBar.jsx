c0abc6afcd02122b3c39f696bf184b38de92716a
jakesondy <87723705+jakesonderman@users.noreply.github.com>
2026-06-11 10:29:58 -0700

    made icon paths less ambiguous and added css styling

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,13 +1,19 @@
 import "./NavBar.css"
 import IconLink from './IconLink.jsx'
+import ytIconJpg from './assets/icons/ogyt.jpg'
+import scIconPng from './assets/icons/ogsc.png'
+import spIconPng from './assets/icons/spotify.png'
+import bcIconPng from './assets/icons/ogbc.png'
+
+
 
 function NavBar() {
     return(
         <div className="btmNavBar">
-            <IconLink img="src/assets/icons/ogyt.jpg" link="https://www.youtube.com/@jakesondy" name="Youtube"/>
-            <IconLink img="src/assets/icons/ogsc.png" link="https://soundcloud.com/jake-sondy?utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing" name="Soundcloud"/>
-            <IconLink img="src/assets/icons/spotify.png" link="https://open.spotify.com/artist/4zXJ9PwydnIEW6dPrzId6t?si=WwxZyArpR-uD_IustBZEmQ" name="Soundcloud"/>
-            <IconLink img="src/assets/icons/ogbc.png" link="https://jakesondy.bandcamp.com/" name="Bandcamp"/>
+            <IconLink img={ytIconJpg} link="https://www.youtube.com/@jakesondy" name="Youtube"/>
+            <IconLink img={scIconPng} link="https://soundcloud.com/jake-sondy?utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing" name="Soundcloud"/>
+            <IconLink img={spIconPng} link="https://open.spotify.com/artist/4zXJ9PwydnIEW6dPrzId6t?si=WwxZyArpR-uD_IustBZEmQ" name="Soundcloud"/>
+            <IconLink img={bcIconPng} link="https://jakesondy.bandcamp.com/" name="Bandcamp"/>
         </div>
     );
 }
