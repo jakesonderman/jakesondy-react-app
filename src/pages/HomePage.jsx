d7436e61130d80e7138281891ca05926a27b95bc
Former Macbook Pro <formermacbookpro@Formers-MacBook-Pro.local>
2026-06-14 22:20:30 -0700

    switched around desktop link positioning

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -16,12 +16,12 @@ export default function HomePage() {
                     WELCOME
                     </h1>
                     <div className="desktop">
-                        <Link to="/shirt" className="page-link page-link-1">
-                            <DeskLink img={JakeSondyFrontPng} name="shirt"/>
-                        </Link>
                         <Link to="https://soundcloud.com/jake-sondy/that-nicotine-remix-12-mp3?si=0ff9c96a86464ab0bf8c95930e0e8170&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing" 
-                        className="page-link page-link-2">
+                        className="page-link page-link-1">
                             <DeskLink img={cdPng} name="fav mix rn"/>
+                        </Link>
+                        <Link to="/shirt" className="page-link page-link-2">
+                            <DeskLink img={JakeSondyFrontPng} name="shirt"/>
                         </Link>
                         <Link to="https://linktr.ee/jake.sondy" 
                         className="page-link page-link-3">
